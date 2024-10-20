0ad27ab747230fd9313745bbc2b4659c47d434b5
dhruvi188 <97607646+dhruvi188@users.noreply.github.com>
2024-10-20 17:17:22 +0530

    Update FirebaseConfig.js

--- src/component/firebaseConfig/FirebaseConfig.js
+++ src/component/firebaseConfig/FirebaseConfig.js
@@ -3,6 +3,9 @@ import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
+
+//I understand that it is not a good practise to keep secrey keys in public url, 
+//but nonetheless I am keeping these for keeping the web-app executable for the evaluation  
 const firebaseConfig = {
   apiKey: "AIzaSyAdFrKiD-8vNCU055xdCqsn9d18mXSeddI",
   authDomain: "fuzzy-invention-5df9c.firebaseapp.com",
@@ -16,7 +19,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 // const provider = new GoogleAuthProvider();
-export const db=getFirestore(app);
+const db=getFirestore(app);
 const database = getDatabase(app);
-export { auth, app, database };
+export { auth, app, database, db };
 
